fc39b92460a8c8dab6afc696a72021f67dddb4b8
n-pretorious <pretoriousndungu@gmail.com>
2020-10-31 22:31:38 +0300

    edit index and profile methods

--- watcher/views.py
+++ watcher/views.py
@@ -1,18 +1,23 @@
-from watcher.models import Profile
+from watcher.models import Neighborhood, Profile
 from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
 def logout(request):
-  logout(request)
-  
+    logout(request)
+
+
 def index(request):
-  return render(request, 'index.html')
+    profile = Profile.objects.get(user=request.user)
+    
+    context = {
+      'profile' : profile
+    }
+    return render(request, "index.html", context)
+
 
 def profile(request):
-  profile = Profile.objects.get(user = request.user)
-  
-  context = {
-    'profile' : profile
-  }
-  return render(request, 'profile.html', context)
+    profile = Profile.objects.get(user=request.user)
+
+    context = {"profile": profile}
+    return render(request, "profile.html", context)
