2cb42f3100ab71fc9dae34ac3904c438c187c234
n-pretorious <pretoriousndungu@gmail.com>
2020-11-02 00:12:56 +0300

    add business view

--- watcher/views.py
+++ watcher/views.py
@@ -1,4 +1,4 @@
-from watcher.models import Neighborhood, Profile
+from watcher.models import Business, Neighborhood, Post, Profile
 from django.http import request
 from django.shortcuts import render
 
@@ -9,10 +9,21 @@ def logout(request):
 
 def index(request):
     profile = Profile.objects.get(user=request.user)
-    
-    context = {
-      'profile' : profile
-    }
+    posts = Post.objects.filter(neighborhood=profile.neighborhood)
+
+    if request.method == "POST":
+        tag = request.POST.get("selectOption")
+        details = request.POST.get("postDetail")
+
+        newPost = Post(
+            tag=tag,
+            details=details,
+            user=request.user,
+            neighborhood=profile.neighborhood,
+        )
+        newPost.save()
+
+    context = {"profile": profile, "posts": posts}
     return render(request, "index.html", context)
 
 
@@ -21,3 +32,25 @@ def profile(request):
 
     context = {"profile": profile}
     return render(request, "profile.html", context)
+
+
+def business(request):
+    profile = Profile.objects.get(user=request.user)
+    businesses = Business.objects.filter(neighborhood=profile.neighborhood)
+    print(businesses)
+
+    if request.method == "POST":
+        name = request.POST.get("businessName")
+        email = request.POST.get("businessEmail")
+
+        newBusiness = Business(
+            name=name, user=request.user, neighborhood=profile.neighborhood, email=email
+        )
+
+        newBusiness.saveBusniess()
+
+    context = {
+        "businesses" : businesses
+    }
+
+    return render(request, "business.html", context)
