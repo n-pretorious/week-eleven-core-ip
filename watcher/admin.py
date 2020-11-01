952592e757f40763aec4da67d0e8cb6a8cb5d826
n-pretorious <pretoriousndungu@gmail.com>
2020-11-02 00:12:30 +0300

    add post & admin

--- watcher/admin.py
+++ watcher/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from watcher.models import Neighborhood, Profile
+from watcher.models import Business, Neighborhood, Post, Profile
 
 # Register your models here.
 admin.site.register(Neighborhood)
 admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Business)
