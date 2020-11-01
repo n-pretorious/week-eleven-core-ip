370897db1e7aabcecf34f72b59e5bce0e1b8b890
n-pretorious <pretoriousndungu@gmail.com>
2020-11-02 00:13:22 +0300

    add business view

--- watcher/urls.py
+++ watcher/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile', views.profile, name='userProfile'),
+    path('business', views.business, name='registerBusiness')
 ]
